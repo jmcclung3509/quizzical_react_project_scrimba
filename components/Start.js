16389f94059e8e83b313396a2c8af65c4d40a73b
Jessica Turner <jmcclung3509@gmail.com>
2022-06-03 13:20:03 -0700

    Changed title

--- components/Start.js
+++ components/Start.js
@@ -5,7 +5,7 @@ export default function Start(props) {
     <div className="wrap-start">
       <div className="start-main">
         <h1> Quizzical</h1>
-        <p className="description"> A fun quiz game to test your knowledge </p>
+        <p className="description"> A fun quiz game to test your comic-book knowledge </p>
         <button className="startBtn" onClick={props.start}>
           start
         </button>
